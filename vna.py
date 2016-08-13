97ac11d5db3d969197e84ec2d8bc2cf6d17a04fe
Gerrit Giehl <gerrit.giehl@email-versand.net>
2016-08-13 18:41:51 +0200

    refactored code

--- vna.py
+++ vna.py
@@ -4,45 +4,54 @@ import csv
 import matplotlib.pyplot as plt
 import numpy as np
 
-ser = serial.Serial(
-        port='/dev/ttyUSB0',
-        baudrate=57600,
-        parity=serial.PARITY_NONE,
-        stopbits=serial.STOPBITS_ONE,
-        bytesize=serial.EIGHTBITS,
-        timeout=5
-        )
+class VnaClient():
 
-start_freq = 23000000
-end_freq = 27000000
+    def __init__(self):
+        self.ser = serial.Serial(
+                #port='/dev/ttyUSB0',
+                port='/dev/pts/5',
+                baudrate=57600,
+                parity=serial.PARITY_NONE,
+                stopbits=serial.STOPBITS_ONE,
+                bytesize=serial.EIGHTBITS,
+                timeout=5
+                )
 
-print('Connection is open:', ser.isOpen())
+        self.start_freq = 23000000
+        self.end_freq = 27000000
 
-s = ser.read(2500)
-#print(s)
+        print('Connection is open:', self.ser.is_open)
 
-ser.write((str(start_freq) + 'a').encode('utf-8'))
-ser.write((str(end_freq) + 'b').encode('utf-8'))
-ser.write('20n'.encode('utf-8'))
+        s = self.ser.read(2500)
+        #print(s)
 
-ser.write('s'.encode('utf-8'))
-s = ser.read(6500)
-data = s.decode('utf-8')
-#print(data)
+        self.ser.write((str(self.start_freq) + 'a').encode('utf-8'))
+        self.ser.write((str(self.end_freq) + 'b').encode('utf-8'))
+        self.ser.write('20n'.encode('utf-8'))
 
-reader = csv.reader(data.splitlines(True), delimiter=',')
-y = []
+    def plot(self):
+        self.ser.write('s'.encode('utf-8'))
+        s = self.ser.read(6500)
+        data = s.decode('utf-8')
+        #print(data)
 
-for row in reader:
-    print(row)
-    if(row[0].upper() != "END"):
-        y.append(row[2])
+        reader = csv.reader(data.splitlines(True), delimiter=',')
+        y = []
 
-ser.close()
+        for row in reader:
+            print(row)
+            if(row[0].upper() != "END"):
+                y.append(row[2])
 
-x = np.arange(start_freq, end_freq, (end_freq - start_freq) / len(y))
+        x = np.arange(self.start_freq, self.end_freq, (self.end_freq - self.start_freq) / len(y))
 
-plt.plot(x, y)
-plt.title('VSWR')
+        plt.plot(x, y)
+        plt.title('VSWR')
 
-plt.show()
+        plt.show()
+
+
+if __name__ == '__main__':
+    vna = VnaClient()
+    vna.plot()
+    vna.ser.close()
